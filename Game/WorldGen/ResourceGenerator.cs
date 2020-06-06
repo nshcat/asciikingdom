a1d1938fd64a7ec13d0b07367dc09ef9f274f95d
nshcat <nshcat@users.noreply.github.com>
2020-06-06 16:33:44 +0200

    Added more jungle resources

--- Game/WorldGen/ResourceGenerator.cs
+++ Game/WorldGen/ResourceGenerator.cs
@@ -99,7 +99,7 @@ namespace Game.WorldGen
         /// </summary>
         protected void GenerateResources()
         {
-            var sampler = new PoissonDiskSampler(this.Dimensions, 7.5f);
+            var sampler = new PoissonDiskSampler(this.Dimensions, 6.5f);
             var rng = new Random(this.Seed + 9118);
             var positions = sampler.Sample(rng);
 
